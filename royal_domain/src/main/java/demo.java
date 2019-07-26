a53e55f78d60242966efeb79806292334821b4a4
wuxinghui <260537614@qq.com>
2019-07-26 10:34:09 +0800

    初始化

---
+++ royal_domain/src/main/java/demo.java
@@ -0,0 +1,9 @@
+/**
+ * @author wuxinghui
+ * @date 2019/7/26-10:32
+ */
+public class demo {
+  public static void main(String[] args) {
+    System.out.println("测试");
+  }
+}
---
+++ royal_parent/src/main/java/test/demo.java
@@ -0,0 +1,8 @@
+package test;
+
+/**
+ * @author wuxinghui
+ * @date 2019/7/26-10:30
+ */
+public class demo {
+}
